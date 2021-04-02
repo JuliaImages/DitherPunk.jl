30d853c4bf14d69f6dc37917f70f7ebfaad57c23
Adrian Hill <adrian.hill@mailbox.org>
2021-04-02 18:02:20 +0200

    Simplify threshold dither and remove sRGB conversion

--- src/mono/threshold.jl
+++ src/mono/threshold.jl
@@ -1,16 +1,7 @@
-function threshold_dithering(
-    img::AbstractMatrix{<:Gray}; threshold=0.5, is_srgb=true
-)::BitMatrix
-    img = copy(img)
-    is_srgb && (img .= srgb2linear.(img))
+function threshold_dithering(img::AbstractMatrix{<:Gray}; threshold=0.5)::BitMatrix
     return img .> threshold
 end
 
-function random_noise_dithering(
-    img::AbstractMatrix{<:Gray}; threshold=0.5, is_srgb=true
-)::BitMatrix
-    img = copy(img)
-    is_srgb && (img .= srgb2linear.(img))
-    img .+= rand(size(img)) .- 0.5
-    return img .> threshold
+function random_noise_dithering(img::AbstractMatrix{<:Gray}; threshold=0.5)::BitMatrix
+    return img .> rand(N0f8, size(img)) # threshold map is random noise
 end

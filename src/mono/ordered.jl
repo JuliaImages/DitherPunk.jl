24ed1c1322e5b6ec7bde1341b11946f0edfda904
Adrian Hill <adrian.hill@mailbox.org>
2021-04-02 18:04:11 +0200

    Fix ordered dithering, remove dependency on ImageFiltering, OffsetArrays
    also remove sRGB conversion

--- src/mono/ordered.jl
+++ src/mono/ordered.jl
@@ -3,46 +3,41 @@ Ordered dithering using threshold maps.
 https://en.wikipedia.org/wiki/Ordered_dithering#Pre-calculated_threshold_maps
 """
 function ordered_dithering(
-    img::BitMatrix, map::AbstractMatrix; is_srgb=true, invert_map=true
-)
-    img = copy(img)
-    map = copy(map)
+    img::AbstractMatrix{<:Gray}, map::AbstractMatrix; invert_map=true
+)::BitMatrix
+    h_img, w_img = size(img)
+    h_map, w_map = size(map)
 
-    is_srgb && (img .= srgb2linear.(img))
+    # create threshold image by repeating map
+    repeat_rows = ceil(Int, h_img / h_map)
+    repeat_cols = ceil(Int, w_img / w_map)
+    threshold = repeat(map, repeat_rows, repeat_cols)[1:h_img, 1:w_img]
 
     # Invert map ∈ [0, 1] if specified
-    invert_map && (map .= 1 .- map)
+    invert_map && (threshold .= 1 .- threshold)
 
-    # Define OffsetArray from map to use with ImageFiltering
-    w = first(size(map)) - 1
-    offsetmap = OffsetArray(map, 0:w, 0:w)
-
-    # Get per-pixel threshold by filtering image with threshold map
-    threshold = imfilter(img, offsetmap)
     return img .> threshold
 end
 
 """
 Ordered dithering using the Bayer matrix as a threshold map.
 """
-function bayer_dithering(
-    img::AbstractMatrix{<:Gray}; level=1, is_srgb=true, invert_map=false
-)::BitMatrix
+function bayer_dithering(img::AbstractMatrix{<:Gray}; level=1, kwargs...)::BitMatrix
     # Get Bayer matri and normalize it to [0, 1]
-    map = bayer(level)
-    map ./= maximum(map)
+    bayer = bayer_matrix(level)
+    map = bayer / maximum(bayer)
 
-    return ordered_dithering(img, map; is_srgb=is_srgb, invert_map=invert_map)
+    return ordered_dithering(img, map; kwargs...)
 end
 
 """
 Contruct (un-normalized) Bayer matrices through recursive definition.
 """
-function bayer(n::Int)::AbstractMatrix{Int}
+function bayer_matrix(n::Int)::AbstractMatrix{Int}
     if n == 0
         b = [0 2; 3 1]
     else
-        bₙ₋₁ = bayer(n - 1)
+        bₙ₋₁ = bayer_matrix(n - 1)
         b = 4 * [(bₙ₋₁) (bₙ₋₁.+2); (bₙ₋₁.+3) (bₙ₋₁.+1)]
     end
     return b

21469e89a0907b1e53199e8f4d70bb3678859662
Adrian Hill <adrian.hill@mailbox.org>
2021-04-02 18:07:54 +0200

    Remove sRGB conversion from gradient test image

--- test/test_gradient.jl
+++ test/test_gradient.jl
@@ -5,9 +5,8 @@ w = 200
 h = 3 * 4 # multiple of 8 for unicode braille print
 
 function gradient_image(height, width)
-    gradient_row = reshape(range(0; stop=1, length=width), 1, width)
-    row = DitherPunk.linear2srgb.(gradient_row)
-    return img = Gray.(vcat(repeat(gradient_row, height)))
+    row = reshape(range(0; stop=1, length=width), 1, width)
+    return img = Gray.(vcat(repeat(row, height)))
 end
 
 img = gradient_image(h, w)

c52c1106e6de34f305bea58637ada908789f7d0c
Adrian Hill <adrian.hill@mailbox.org>
2021-04-12 22:32:32 +0200

    Update logo to be more readable

--- docs/logo/make_logo.jl
+++ docs/logo/make_logo.jl
@@ -5,9 +5,9 @@ using ImageTransformations
 using ImageContrastAdjustment
 
 img_color = load("./docs/logo/DitherPunk.png")
-img_color = imresize(img_color; ratio=1 / 4)
+img_color = imresize(img_color; ratio=1 / 5)
 img = Gray.(img_color)
 img = adjust_histogram(img, LinearStretching())
 
 dither = rhombus_dithering(img);
-save("./docs/logo/DitheredPunk.png", show_dither(dither; scale=2))
+save("./docs/logo/DitheredPunk.png", show_dither(dither; scale=3))

3bc6fe46ed51a1f8ed301fc46dc0f2e148c641dd
Adrian Hill <adrian.hill@mailbox.org>
2021-04-14 10:46:51 +0200

    Comment what test_on_gradient does

--- docs/literate/gallery.jl
+++ docs/literate/gallery.jl
@@ -4,21 +4,24 @@ using Images
 # # Gallery
 # A simple linear gradient works well to reveal the characteristic patterns of
 # different dithering algorithms.
-#
+img = gradient_image(100, 800)
+
+# The function `test_on_gradient` takes a dithering algorithm and runs it on this
+# image, showing the input and the output.
+
 # ## Threshold dithering
 # ### `threshold_dithering`
-threshold_dithering |> test_on_gradient
+test_on_gradient(threshold_dithering)
 
 # ### `random_noise_dithering`
-random_noise_dithering |> test_on_gradient
+test_on_gradient(random_noise_dithering)
 
 # ## Ordered dithering on small images
 # ### `bayer_dithering`
-bayer_dithering |> test_on_gradient
+test_on_gradient(bayer_dithering)
 
 # The order of the Bayer-matrix can be specified through the parameter `level`,
 # which defaults to `1`.
-img = gradient_image(100, 800)
 dithers = [Gray.(bayer_dithering(img; level=level)) for level in 0:4]
 mosaicview([img, dithers...])
 
@@ -27,10 +30,10 @@ mosaicview([img, dithers...])
 # better suited for large images.
 #
 # ### `clustered_dots_dithering`
-clustered_dots_dithering |> test_on_gradient
+test_on_gradient(clustered_dots_dithering)
 
 # ### `balanced_centered_point_dithering`
-balanced_centered_point_dithering |> test_on_gradient
+test_on_gradient(balanced_centered_point_dithering)
 
 # ### `rhombus_dithering`
-rhombus_dithering |> test_on_gradient
+test_on_gradient(rhombus_dithering)

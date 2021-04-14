c6e7e93cd8aa082636a73428e78c590505a786a0
Documenter.jl <documenter@juliadocs.github.io>
2021-04-14 15:39:12 +0000

    build based on e34a4a0

--- dev/generated/simple_example.jl
+++ dev/generated/simple_example.jl
@@ -13,8 +13,11 @@ dither = balanced_centered_point_dithering(img);
 
 Gray.(dither)
 
+dither = balanced_centered_point_dithering(img, to_linear=true)
+Gray.(dither)
+
 img = imresize(img; ratio=1 / 5) # downscale
-dither = bayer_dithering(img);
+dither = bayer_dithering(img, to_linear=true);
 
 show_dither(dither; scale=3)
 

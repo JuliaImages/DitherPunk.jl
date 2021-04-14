021b7d29d3f45fba8feff46e06cfc9e250d5d222
Documenter.jl <documenter@juliadocs.github.io>
2021-04-14 15:14:44 +0000

    build based on a332258

--- dev/generated/gallery.jl
+++ dev/generated/gallery.jl
@@ -1,21 +1,23 @@
 using DitherPunk
 using Images
 
-threshold_dithering |> test_on_gradient
+srbg, linear = gradient_image(100, 800);
+mosaicview(srbg, linear)
 
-random_noise_dithering |> test_on_gradient
+test_on_gradient(threshold_dithering)
 
-bayer_dithering |> test_on_gradient
+test_on_gradient(white_noise_dithering)
 
-img = gradient_image(100, 800)
-dithers = [Gray.(bayer_dithering(img; level=level)) for level in 0:4]
-mosaicview([img, dithers...])
+test_on_gradient(bayer_dithering)
 
-clustered_dots_dithering |> test_on_gradient
+bayer_dithering_algs = [(img) -> (bayer_dithering(img; level=level)) for level in 4:-1:0]
+test_on_gradient(bayer_dithering_algs)
 
-balanced_centered_point_dithering |> test_on_gradient
+test_on_gradient(clustered_dots_dithering)
 
-rhombus_dithering |> test_on_gradient
+test_on_gradient(balanced_centered_point_dithering)
+
+test_on_gradient(rhombus_dithering)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 

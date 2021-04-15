8606b42d3d2ec075cd129b705216c3a56655b76f
Adrian Hill <adrian.hill@mailbox.org>
2021-04-15 19:04:58 +0200

    Remove normalization and select color space

--- docs/literate/gallery_images.jl
+++ docs/literate/gallery_images.jl
@@ -8,7 +8,6 @@ using TestImages
 # This gallery uses images from [*TestImages.jl*](https://testimages.juliaimages.org).
 function preprocess(img)
     img = Gray.(img)
-    img .= adjust_histogram(img, LinearStretching()) # normalize
     return imresize(img; ratio=1 / 2)
 end
 
@@ -20,8 +19,8 @@ mosaicview(imgs...; ncol=3)
 
 # Our test function `test_on_images` just runs a dithering algorithm on all six images
 # in linear color space (`to_liner=true`).
-function test_on_images(alg)
-    dithers = [Gray.(alg(img; to_linear=true)) for img in imgs]
+function test_on_images(alg; to_linear=false)
+    dithers = [Gray.(alg(img; to_linear)) for img in imgs]
     return mosaicview(dithers...; ncol=3)
 end
 
@@ -30,19 +29,20 @@ end
 test_on_images(threshold_dithering)
 
 # #### `white_noise_dithering`
-test_on_images(white_noise_dithering)
+test_on_images(white_noise_dithering; to_linear=true)
 
 # ## Ordered dithering on small images
 # #### Bayer matrices
+test_on_images(bayer_dithering)
 
 # #### `clustered_dots_dithering`
-test_on_images(clustered_dots_dithering)
+test_on_images(clustered_dots_dithering; to_linear=true)
 
 # #### `balanced_centered_point_dithering`
-test_on_images(balanced_centered_point_dithering)
+test_on_images(balanced_centered_point_dithering; to_linear=true)
 
 # #### `rhombus_dithering`
-test_on_images(rhombus_dithering)
+test_on_images(rhombus_dithering; to_linear=true)
 
 # ## Error diffusion
 # #### `simple_error_diffusion`

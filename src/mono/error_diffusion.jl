b7871bf8c4396a1d339ba4fdf7a55362713e0e65
Adrian Hill <adrian.hill@mailbox.org>
2021-04-18 02:37:41 +0200

    Fix bug in error diffusion OffsetArray indices

--- src/mono/error_diffusion.jl
+++ src/mono/error_diffusion.jl
@@ -50,28 +50,30 @@ function indices(om::OffsetMatrix)
 end
 
 simple_error_diffusion(img; kwargs...) = error_diffusion(img, SIMPLE_STENCIL; kwargs...)
-SIMPLE_STENCIL = OffsetArray([0 0.5; 0.5 0], 1:2, 1:2)
+SIMPLE_STENCIL = OffsetArray([0 1; 1 0]//2, 0:1, 0:1)
 
 floyd_steinberg_diffusion(img; kwargs...) = error_diffusion(img, FS_STENCIL; kwargs...)
-FS_STENCIL = OffsetArray([0 0 7; 3 5 1]//16, 1:2, -1:1)
+FS_STENCIL = OffsetArray([0 0 7; 3 5 1]//16, 0:1, -1:1)
 
 jarvis_judice_diffusion(img; kwargs...) = error_diffusion(img, JJ_STENCIL; kwargs...)
-JJ_STENCIL = OffsetArray([0 0 0 7 5; 3 5 7 5 3; 1 3 5 3 1]//48, 1:3, -2:2)
+JJ_STENCIL = OffsetArray([0 0 0 7 5; 3 5 7 5 3; 1 3 5 3 1]//48, 0:2, -2:2)
 
 stucki_diffusion(img; kwargs...) = error_diffusion(img, STUCKI_STENCIL; kwargs...)
-STUCKI_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2; 1 2 4 2 1]//42, 1:3, -2:2)
+STUCKI_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2; 1 2 4 2 1]//42, 0:2, -2:2)
 
 burkes_diffusion(img; kwargs...) = error_diffusion(img, BURKES_STENCIL; kwargs...)
-BURKES_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2]//32, 1:2, -2:2)
+BURKES_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2]//32, 0:1, -2:2)
 
 sierra_diffusion(img; kwargs...) = error_diffusion(img, SIERRA_STENCIL; kwargs...)
-SIERRA_STENCIL = OffsetArray([0 0 0 5 3; 2 4 5 4 2; 0 2 3 2 0]//32, 1:3, -2:2)
+SIERRA_STENCIL = OffsetArray([0 0 0 5 3; 2 4 5 4 2; 0 2 3 2 0]//32, 0:2, -2:2)
 
-two_row_sierra_diffusion(img; kwargs...) = error_diffusion(img, TWO_ROW_SIERRA_STENCIL; kwargs...)
-TWO_ROW_SIERRA_STENCIL = OffsetArray([0 0 0 4 3; 1 2 3 2 1]//16, 1:2, -2:2)
+function two_row_sierra_diffusion(img; kwargs...)
+    return error_diffusion(img, TWO_ROW_SIERRA_STENCIL; kwargs...)
+end
+TWO_ROW_SIERRA_STENCIL = OffsetArray([0 0 0 4 3; 1 2 3 2 1]//16, 0:1, -2:2)
 
 sierra_lite_diffusion(img; kwargs...) = error_diffusion(img, SIERRA_LITE_STENCIL; kwargs...)
-SIERRA_LITE_STENCIL = OffsetArray([0 0 2; 1 1 0]//4, 1:2, -1:1)
+SIERRA_LITE_STENCIL = OffsetArray([0 0 2; 1 1 0]//4, 0:1, -1:1)
 
 atkinson_diffusion(img; kwargs...) = error_diffusion(img, ATKINSON_STENCIL; kwargs...)
-ATKINSON_STENCIL = OffsetArray([0 0 1 1; 1 1 1 0; 0 1 0 0]//8, 1:3, -1:2)
+ATKINSON_STENCIL = OffsetArray([0 0 1 1; 1 1 1 0; 0 1 0 0]//8, 0:2, -1:2)

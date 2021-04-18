85af0df06d013f92f041e4f0cbd8a4e66ad4750e
Documenter.jl <documenter@juliadocs.github.io>
2021-04-18 01:03:30 +0000

    build based on b7871bf

---
+++ v0.2.2/generated/gallery_gradient.jl
@@ -0,0 +1,41 @@
+using DitherPunk
+using Images
+
+srbg, linear = gradient_image(100, 800);
+mosaicview(srbg, linear)
+
+test_on_gradient(threshold_dithering)
+
+test_on_gradient(white_noise_dithering)
+
+test_on_gradient(bayer_dithering)
+
+bayer_dithering_algs = [(img) -> (bayer_dithering(img; level=level)) for level in 4:-1:0]
+test_on_gradient(bayer_dithering_algs)
+
+test_on_gradient(clustered_dots_dithering)
+
+test_on_gradient(balanced_centered_point_dithering)
+
+test_on_gradient(rhombus_dithering)
+
+test_on_gradient(simple_error_diffusion)
+
+test_on_gradient(floyd_steinberg_diffusion)
+
+test_on_gradient(jarvis_judice_diffusion)
+
+test_on_gradient(stucki_diffusion)
+
+test_on_gradient(burkes_diffusion)
+
+test_on_gradient(sierra_diffusion)
+
+test_on_gradient(two_row_sierra_diffusion)
+
+test_on_gradient(sierra_lite_diffusion)
+
+test_on_gradient(atkinson_diffusion)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+

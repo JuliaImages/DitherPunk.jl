468c36016cba63b8bb733bff2db30f8f2456f73e
Adrian Hill <adrian.hill@mailbox.org>
2021-05-03 10:24:59 +0200

    Drop Julia 1.0 and 1.1 from CI
    and remove SafeTestsets as it doesn't seem necessary.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using DitherPunk
 using Test
-using SafeTestsets
 
-@time @safetestset "DitherPunk.jl" begin
-    @time @safetestset "Gradient image" begin
+@testset "DitherPunk.jl" begin
+    @testset "Bayer matrices" begin
         include("test_bayer.jl")
+    end
+    @testset "Gradient image" begin
         include("test_gradient.jl")
     end
 end

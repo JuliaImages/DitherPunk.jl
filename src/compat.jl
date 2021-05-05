5f82b2317179169f71a5b4a9afcb21308ee3a976
Adrian Hill <adrian.hill@mailbox.org>
2021-05-05 20:35:46 +0200

    Format code

--- src/compat.jl
+++ src/compat.jl
@@ -2,5 +2,5 @@ if isdefined(OffsetArrays, :OffsetMatrix)
     # This alias is introduced in OffsetArrays v1.1.3
     OffsetMatrix = OffsetArrays.OffsetMatrix
 else
-    const OffsetMatrix{T} = OffsetArrays.OffsetArray{T, 2}
+    const OffsetMatrix{T} = OffsetArrays.OffsetArray{T,2}
 end

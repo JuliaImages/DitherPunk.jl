b80ed2312e1efcd3cc7f987886687bb4e7dfd09d
Adrian Hill <adrian.hill@mailbox.org>
2021-05-14 02:42:39 +0200

    Generalize error diffusion to color algorithms and use new `dither` API

--- src/error_diffusion.jl
+++ src/error_diffusion.jl
@@ -1,38 +1,55 @@
-function error_diffusion(
-    img::AbstractMatrix{<:Gray}, stencil::OffsetMatrix; to_linear=false
-)::BitMatrix
+struct ErrorDiffusion{S,RR,CR} <: AbstractDither
+    stencil::S
+    rowrange::RR
+    colrange::CR
+
+    function ErrorDiffusion(stencil, rowrange, colrange)
+        stencil = OffsetMatrix(stencil, rowrange, colrange)
+        return new{typeof(stencil),typeof(rowrange),typeof(colrange)}(
+            stencil, rowrange, colrange
+        )
+    end
+end
+
+function dither(
+    img::AbstractMatrix{T},
+    alg::ErrorDiffusion,
+    cs::ColorScheme;
+    to_linear=false,
+    metric::DifferenceMetric=DE_2000(),
+)::AbstractMatrix{T} where {T<:Color}
     # this function does not yet support OffsetArray
     require_one_based_indexing(img)
 
+    length(cs) >= 2 ||
+        throw(DomainError(steps, "Color scheme for dither needs >= 2 colors."))
+
     # Change from normalized intensities to Float as error will get added!
-    FT = floattype(eltype(img))
+    FT = floattype(T)
     if to_linear
         _img = @. FT(srgb2linear(img))
     else
         _img = FT.(img)
     end
-    stencil = FT.(stencil) # eagerly promote to the same type to make loop run faster
+    stencil = alg.stencil # eagerly promote to the same type to make loop run faster
 
     h, w = size(_img)
-    dither = BitArray(undef, h, w) # initialized to zero
-
-    # Get OffsetMatrix indices
-    drs, dcs = indices(stencil)
+    dither = zeros(FT, h, w) # initialized to zero
 
     @inbounds for r in 1:h
         for c in 1:w
             px = _img[r, c]
 
             # Round to closest color
-            px >= 0.5 ? (rnd = 1) : (rnd = 0)
+            col = closest_color(px, cs; metric)
 
             # Apply pixel to dither
-            dither[r, c] = rnd
+            dither[r, c] = col
 
             # Diffuse "error" to neighborhood in stencil
-            err = px - rnd
-            for dr in drs
-                for dc in dcs
+            err = px - col
+            for dr in alg.rowrange
+                for dc in alg.colrange
                     if (r + dr > 0) && (r + dr <= h) && (c + dc > 0) && (c + dc <= w)
                         _img[r + dr, c + dc] += err * stencil[dr, dc]
                     end
@@ -45,40 +62,37 @@ function error_diffusion(
 end
 
 """
-Get indices from OffsetMatrix
+Define custom color difference metric with linear distances between grayscale values.
 """
-function indices(om::OffsetMatrix)
-    rows, cols = size(om)
-    row_range = (1:rows) .+ om.offsets[1]
-    col_range = (1:cols) .+ om.offsets[2]
-    return row_range, col_range
+struct BinaryDitherMetric <: DifferenceMetric end
+
+function ImageCore.Colors._colordiff(a::Color, b::Color, m::BinaryDitherMetric)
+    return abs(a - b)
 end
 
-simple_error_diffusion(img; kwargs...) = error_diffusion(img, SIMPLE_STENCIL; kwargs...)
-const SIMPLE_STENCIL = OffsetArray([0 1; 1 0]//2, 0:1, 0:1)
-
-floyd_steinberg_diffusion(img; kwargs...) = error_diffusion(img, FS_STENCIL; kwargs...)
-const FS_STENCIL = OffsetArray([0 0 7; 3 5 1]//16, 0:1, -1:1)
-
-jarvis_judice_diffusion(img; kwargs...) = error_diffusion(img, JJ_STENCIL; kwargs...)
-const JJ_STENCIL = OffsetArray([0 0 0 7 5; 3 5 7 5 3; 1 3 5 3 1]//48, 0:2, -2:2)
-
-stucki_diffusion(img; kwargs...) = error_diffusion(img, STUCKI_STENCIL; kwargs...)
-const STUCKI_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2; 1 2 4 2 1]//42, 0:2, -2:2)
-
-burkes_diffusion(img; kwargs...) = error_diffusion(img, BURKES_STENCIL; kwargs...)
-const BURKES_STENCIL = OffsetArray([0 0 0 8 4; 2 4 8 4 2]//32, 0:1, -2:2)
-
-sierra_diffusion(img; kwargs...) = error_diffusion(img, SIERRA_STENCIL; kwargs...)
-const SIERRA_STENCIL = OffsetArray([0 0 0 5 3; 2 4 5 4 2; 0 2 3 2 0]//32, 0:2, -2:2)
-
-function two_row_sierra_diffusion(img; kwargs...)
-    return error_diffusion(img, TWO_ROW_SIERRA_STENCIL; kwargs...)
+function dither(
+    img::AbstractMatrix{<:AbstractGray},
+    alg::ErrorDiffusion;
+    steps=2,
+    metric=BinaryDitherMetric(),
+    kwargs...,
+)
+    # Construct grayscale ColorScheme of length `steps`
+    cs = ColorScheme([Gray(i) for i in range(0.0, 1.0; length=steps)])
+    d = dither(img, alg, cs; metric, kwargs...)
+    if steps == 2
+        # match return type of other binary dithering algorithms
+        return BitMatrix(d)
+    end
+    return d
 end
-const TWO_ROW_SIERRA_STENCIL = OffsetArray([0 0 0 4 3; 1 2 3 2 1]//16, 0:1, -2:2)
 
-sierra_lite_diffusion(img; kwargs...) = error_diffusion(img, SIERRA_LITE_STENCIL; kwargs...)
-const SIERRA_LITE_STENCIL = OffsetArray([0 0 2; 1 1 0]//4, 0:1, -1:1)
-
-atkinson_diffusion(img; kwargs...) = error_diffusion(img, ATKINSON_STENCIL; kwargs...)
-const ATKINSON_STENCIL = OffsetArray([0 0 1 1; 1 1 1 0; 0 1 0 0]//8, 0:2, -1:2)
+SimpleErrorDiffusion() = ErrorDiffusion([0 1; 1 0]//2, 0:1, 0:1)
+FloydSteinberg() = ErrorDiffusion([0 0 7; 3 5 1]//16, 0:1, -1:1)
+JarvisJudice() = ErrorDiffusion([0 0 0 7 5; 3 5 7 5 3; 1 3 5 3 1]//48, 0:2, -2:2)
+Stucki() = ErrorDiffusion([0 0 0 8 4; 2 4 8 4 2; 1 2 4 2 1]//42, 0:2, -2:2)
+Burkes() = ErrorDiffusion([0 0 0 8 4; 2 4 8 4 2]//32, 0:1, -2:2)
+Sierra() = ErrorDiffusion([0 0 0 5 3; 2 4 5 4 2; 0 2 3 2 0]//32, 0:2, -2:2)
+TwoRowSierra() = ErrorDiffusion([0 0 0 4 3; 1 2 3 2 1]//16, 0:1, -2:2)
+SierraLite() = ErrorDiffusion([0 0 2; 1 1 0]//4, 0:1, -1:1)
+Atkinson() = ErrorDiffusion([0 0 1 1; 1 1 1 0; 0 1 0 0]//8, 0:2, -1:2)

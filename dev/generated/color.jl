09b2fc18d2f5cfe2b427192a8cc2ef9ade5565d6
Documenter.jl <documenter@juliadocs.github.io>
2021-06-13 11:03:52 +0000

    build based on b342bf9

--- dev/generated/color.jl
+++ dev/generated/color.jl
@@ -1,6 +1,5 @@
 using DitherPunk
 using Images
-using ColorSchemes
 using TestImages
 
 white = RGB{Float32}(1, 1, 1)
@@ -10,12 +9,21 @@ orange = RGB{Float32}(1, 0.5, 0)
 red = RGB{Float32}(1, 0, 0)
 blue = RGB{Float32}(0, 0, 1)
 
-rubiks_colors = ColorScheme([white, yellow, green, orange, red, blue])
+rubiks_colors = [white, yellow, green, orange, red, blue]
 
 img = testimage("fabio_color_256")
 img = imresize(img, 60, 60)
 
 d = dither(img, FloydSteinberg(), rubiks_colors)
+
+d = dither(img, ClosestColor(), rubiks_colors)
+
+using ColorSchemes
+cs = ColorSchemes.flag_br
+colors = cs.colors
+
+img = testimage("fabio_color_256")
+d = dither(img, Atkinson(), colors)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 

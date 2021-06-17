b694b3ad1ef40065a57d35fb0f62f93d51c97b03
Adrian Hill <adrian.hill@mailbox.org>
2021-06-17 20:53:06 +0200

    Add references to docstrings
    closes #19
    Also renames `stencil` to `filter`, as the literature typically refers to "error filters".

--- src/ordered.jl
+++ src/ordered.jl
@@ -45,6 +45,10 @@ end
 Ordered dithering using the Bayer matrix as a threshold matrix.
 The Bayer matrix is of dimension ``2^{n+1} \\times 2^{n+1}``, where ``n`` is the `level`,
 which defaults to `1`.
+
+[1]  Bayer, B.E., "An Optimum Method for Two-Level Rendition of Continuous
+     Tone Pictures," IEEE International Conference on Communications,
+     Conference Records, 1973, pp. 26-11 to 26-15.
 """
 function Bayer(; level=1)
     bayer = bayer_matrix(level)

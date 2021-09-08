e3e7dbd63298f05f30aa54a1c501d31f326a0ee8
Adrian Hill <adrian.hill@mailbox.org>
2021-09-08 22:35:18 +0200

    Fix doc strings

--- src/error_diffusion.jl
+++ src/error_diffusion.jl
@@ -85,6 +85,19 @@ function (alg::ErrorDiffusion)(out::GenericGrayImage,img::GenericGrayImage)
     return out
 end
 
+"""
+    SimpleErrorDiffusion()
+
+Error diffusion algorithm using the filter
+```
+*   1
+1   0         (1//2)
+```
+
+[1]  Floyd, R.W. and L. Steinberg, "An Adaptive Algorithm for Spatial Gray
+     Scale."  SID 1975, International Symposium Digest of Technical Papers,
+     vol 1975m, pp. 36-37.
+"""
 SimpleErrorDiffusion() = ErrorDiffusion(OffsetMatrix([0 1; 1 0]//2, 0:1, 0:1))
 
 """

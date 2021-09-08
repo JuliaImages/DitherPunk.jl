1aa2d1053933634d087b39956b4f1b3554e3fef9
Documenter.jl <documenter@juliadocs.github.io>
2021-09-08 20:56:43 +0000

    build based on 45c7ae0

--- dev/generated/simple_example.jl
+++ dev/generated/simple_example.jl
@@ -1,21 +1,41 @@
 using DitherPunk
 using Images
-using Images: ImageContrastAdjustment
 using TestImages
 
-img = testimage("lighthouse")
+img_color = testimage("lighthouse")
+img_color = imresize(img_color; ratio = 1//2)
+img_gray = Gray.(img_color) # covert to grayscale
 
-img = Gray.(img) # covert to grayscale
-img = adjust_histogram(img, LinearStretching()) # normalize
+dither(img_gray, Bayer())
 
-d = dither(img, BalancedCenteredPoint())
+dither(img_gray, Bayer(); to_linear=true)
 
-d = dither(img, BalancedCenteredPoint(); to_linear=true)
+dither(img_color, SeparateSpace(Bayer()))
 
-img = imresize(img; ratio=1 / 5) # downscale
-d = dither(img, Bayer(); to_linear=true)
+dither(img_color, SeparateSpace(FloydSteinberg()))
 
-upscale(d, 3)
+dither(img_color, SeparateSpace(Rhombus()))
+
+white = RGB{Float32}(1, 1, 1)
+yellow = RGB{Float32}(1, 1, 0)
+green = RGB{Float32}(0, 0.5, 0)
+orange = RGB{Float32}(1, 0.5, 0)
+red = RGB{Float32}(1, 0, 0)
+blue = RGB{Float32}(0, 0, 1)
+
+rubiks_colors = [white, yellow, green, orange, red, blue]
+
+img = testimage("fabio_color_256")
+img = imresize(img, 150, 150)
+
+d = dither(img, FloydSteinberg(), rubiks_colors)
+
+d = dither(img, ClosestColor(), rubiks_colors)
+
+using ColorSchemes
+cs = ColorSchemes.flag_br
+
+dither(img, Atkinson(), cs.colors)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 

a9bdc6efbcff6ad375e24bff94dbfabe4ee93614
Adrian Hill <adrian.hill@mailbox.org>
2021-09-12 19:11:00 +0200

    Add benchmarks (#41)

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,48 @@
+using BenchmarkTools
+using DitherPunk
+using TestImages
+using ImageCore
+
+on_CI = haskey(ENV, "GITHUB_ACTIONS")
+
+img_gray = testimage("fabio_gray_256")
+img_color = testimage("fabio_color_256")
+
+## Define color scheme
+white = RGB{Float32}(1, 1, 1)
+yellow = RGB{Float32}(1, 1, 0)
+green = RGB{Float32}(0, 0.5, 0)
+orange = RGB{Float32}(1, 0.5, 0)
+red = RGB{Float32}(1, 0, 0)
+blue = RGB{Float32}(0, 0, 1)
+cs = [white, yellow, green, orange, red, blue]
+
+# Use one representative algorithm of each type
+algs = Dict(
+    "error diffusion" => FloydSteinberg(),
+    "ordered dithering" => Bayer(; level=3),
+    "closest color" => ClosestColor(),
+    "threshold dithering" => WhiteNoiseThreshold(),
+)
+
+# Tag what is tested on each algorithm type
+SUITE = BenchmarkGroup()
+SUITE["error diffusion"] = BenchmarkGroup(["binary", "color"])
+SUITE["ordered dithering"] = BenchmarkGroup(["binary"])
+SUITE["threshold dithering"] = BenchmarkGroup(["binary"])
+SUITE["closest color"] = BenchmarkGroup(["binary", "color"])
+
+println(SUITE)
+
+for (algname, alg) in algs
+    SUITE[algname]["binary new"] = @benchmarkable dither($(img_gray), $(alg))
+    SUITE[algname]["binary inplace"] = @benchmarkable dither!($(copy(img_gray)), $(alg))
+
+    SUITE[algname]["per-channel new"] = @benchmarkable dither($(img_color), SeparateSpace($(alg)))
+    SUITE[algname]["per-channel inplace"] = @benchmarkable dither!($(copy(img_color)), SeparateSpace($(alg)))
+
+    if "color" in SUITE[algname].tags
+        SUITE[algname]["color new"] = @benchmarkable dither($(img_color), $(alg), $(cs))
+        SUITE[algname]["color inplace"] = @benchmarkable dither!($(copy(img_color)), $(alg), $(cs))
+    end
+end

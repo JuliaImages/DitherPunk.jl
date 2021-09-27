464dcf6811c3db039c539c020abe0079b56743e6
Adrian Hill <adrian.hill@mailbox.org>
2021-09-28 00:38:18 +0200

    Update tests

--- test/test_color.jl
+++ test/test_color.jl
@@ -21,23 +21,41 @@ cs = [white, yellow, green, orange, red, blue]
 
 # Load test image
 img = testimage("fabio_color_256")
+img_gray = testimage("fabio_gray_256")
 imshow(img)
 
 # Run & test custom color pallete dithering methods
 algs = Dict("FloydSteinberg" => FloydSteinberg(), "ClosestColor" => ClosestColor())
 
 for (name, alg) in algs
+    # Test custom color dithering on color images
     img2 = copy(img)
     d = dither(img2, alg, cs)
     @test_reference "references/color_$(name).txt" d
     @test eltype(d) == eltype(img2)
     @test img2 == img # image not modified
-
     imshow(d)
 
-    d = dither!(img2, alg, cs)
-    @test eltype(d) == eltype(img)
+    dither!(img2, alg, cs)
+    @test eltype(img2) == eltype(img)
     @test img2 == d # image updated in-place
+
+    # Test custom color dithering on gray images
+    img2_gray = copy(img_gray)
+    d = dither(img2_gray, alg, cs)
+    @test_reference "references/color_from_gray_$(name).txt" d
+    @test eltype(d) == eltype(cs)
+    @test img2_gray == img_gray # image not modified
+    imshow(d)
+
+    dither!(img2_gray, alg, cs)
+    @test eltype(img2_gray) == eltype(img_gray)
+    imshow(img2_gray)
+end
+
+# Test for argument errors on AbstractBinaryDither algorithms
+for alg in [Bayer(), WhiteNoiseThreshold(), ConstantThreshold()]
+    @test_throws ArgumentError dither(img, alg, cs)
 end
 
 # Test conditional dependency on ColorSchemes.jl

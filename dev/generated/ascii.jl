23e7348400e6c4e9e7b7ca7e7271218804214cb5
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 20:40:36 +0000

    build based on ab48da1

---
+++ dev/generated/ascii.jl
@@ -0,0 +1,20 @@
+using Images
+using DitherPunk
+using TestImages
+
+img = testimage("cameraman")
+img = imresize(img, ratio=(1//14, 1//6))
+
+ascii_ramp = split(" .:-=+*#%@", "")
+cs = Gray.(range(0, 1, length=10))
+
+d = dither(img, FloydSteinberg(), cs)
+
+mat = ascii_ramp[d.index]
+
+for r in eachrow(mat)
+    println(join(r))
+end
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+

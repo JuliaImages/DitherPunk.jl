a79e115bc02860b50d8c449bbde56d42f416b390
Adrian Hill <adrian.hill@mailbox.org>
2022-03-22 00:50:42 +0100

    Attempt at type stability fix

--- src/colorschemes.jl
+++ src/colorschemes.jl
@@ -1,9 +1,9 @@
 # These functions are only conditionally loaded with ColorSchemes.jl
 function _colordither(T, img, alg, cs::ColorSchemes.ColorScheme; kwargs...)
-    return _colordither(T, img, alg, cs.colors)
+    return _colordither(T, img, alg, cs.colors; kwargs...)
 end
 
 function _colordither(T, img, alg, csname::Symbol; kwargs...)
-    cs = ColorSchemes.colorschemes[csname]
-    return _colordither(T, img, alg, cs.colors; kwargs...)
+    cs::ColorSchemes.ColorScheme = ColorSchemes.colorschemes[csname]
+    return _colordither(T, img, alg, cs; kwargs...)
 end

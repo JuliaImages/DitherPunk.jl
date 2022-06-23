a848717c7c612078d5075070f807c7662985d368
Adrian Hill <adrian.hill@mailbox.org>
2022-06-23 23:27:25 +0200

    Remove failing deprecations

--- src/deprecations.jl
+++
@@ -1,2 +0,0 @@
-# v2
-# @deprecate ErrorDiffusion(filter, offset; color_space=XYZ, kwargs...) ErrorDiffusion(filter, offset, color_space; kwargs...)
--- test/deprecations.jl
+++
@@ -1,4 +0,0 @@
-# @testset "test deprecated usage" begin
-#     alg = FloydSteinberg(RGB)
-#     @test alg == ErrorDiffusion(DitherPunk.FLOYD_STEINBERG, 2; color_space=RGB)
-# end

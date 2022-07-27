af491691436d92050ae7230c24e0ffcb4fc8be85
Documenter.jl <documenter@juliadocs.github.io>
2022-07-27 02:34:08 +0000

    build based on d652265

---
+++ v3.0.1/generated/palette_swapping.jl
@@ -0,0 +1,45 @@
+using Images
+using DitherPunk
+using IndirectArrays
+using TestImages
+
+img = testimage("peppers")
+img_secret = testimage("airplaneF16");
+
+ncolors = 8
+d = dither(img, FloydSteinberg(), ncolors)
+
+d_secret = dither(img_secret, FloydSteinberg(), ncolors)
+
+d.values
+
+d.index
+
+combineindex(a, b) = UInt8((a - 1) * ncolors + b)
+function uncombineindex(c)
+    b = (c - 1) % ncolors + 1
+    a = Int((c - b) / ncolors) + 1
+    return a, b
+end
+
+function combine_images(ia1::IndirectArray, ia2::IndirectArray)
+    index = map(t -> combineindex(t...), zip(ia1.index, ia2.index))
+    cs1 = similar(ia1.values, ncolors^2)
+    cs2 = similar(ia2.values, ncolors^2)
+
+    for i in 1:(ncolors^2)
+        a, b = uncombineindex(i)
+        cs1[i] = ia1.values[a]
+        cs2[i] = ia2.values[b]
+    end
+    return index, cs1, cs2
+end;
+
+index, cs, cs_secret = combine_images(d, d_secret);
+
+IndirectArray(index, cs)
+
+IndirectArray(index, cs_secret)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+

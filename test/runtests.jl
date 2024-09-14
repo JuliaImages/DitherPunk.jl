35068f304b26938f6f8a452942054d9659b5cebb
Adrian Hill <adrian.hill@mailbox.org>
2024-09-14 16:56:39 +0200

    Refactor `OrderedDithering` struct (#104)
    * Refactor ordered dithering structs
    
    * Allow any `AbstractMatrix`
    
    * Refactor tests on grayscale gradient

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,6 @@
 using DitherPunk
 using Test
 using Aqua
-using ImageBase: Gray
-
-function gradient_image(height, width)
-    row = reshape(range(0; stop=1, length=width), 1, width)
-    grad = Gray.(vcat(repeat(row, height))) # Linear gradient
-    img = srgb2linear.(grad) # For printing, compensate for SRGB colorspace
-    return grad, img
-end
 
 @testset "DitherPunk.jl" begin
     @testset "Aqua.jl" begin
